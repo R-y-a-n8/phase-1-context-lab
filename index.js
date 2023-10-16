af663f14bf442399295658701912e3f6f0605f55
devRyan <ryan.kinuthia@student.moringaschool.com>
2023-10-16 16:13:50 +0300

    context

--- index.js
+++ index.js
@@ -1,4 +1,55 @@
 /* Your Code Here */
+function createEmployeeRecord(firstName, familyName, title, payPerHour) {
+    return {
+      firstName,
+      familyName,
+      title,
+      payPerHour,
+      timeInEvents: [],
+      timeOutEvents: [],
+    };
+  }
+  function createEmployeeRecords(employeeData) {
+    return employeeData.map((employee) => createEmployeeRecord(...employee));
+  }
+  function createTimeInEvent(employee, dateStamp) {
+    const [date, hour] = dateStamp.split(' ');
+    employee.timeInEvents.push({
+      type: 'TimeIn',
+      date,
+      hour: parseInt(hour, 10),
+    });
+    return employee;
+  }
+  function createTimeOutEvent(employee, dateStamp) {
+    const [date, hour] = dateStamp.split(' ');
+    employee.timeOutEvents.push({
+      type: 'TimeOut',
+      date,
+      hour: parseInt(hour, 10),
+    });
+    return employee;
+  }
+  function hoursWorkedOnDate(employee, date) {
+    const timeIn = employee.timeInEvents.find((event) => event.date === date);
+    const timeOut = employee.timeOutEvents.find((event) => event.date === date);
+    return (timeOut.hour - timeIn.hour) / 100;
+  }
+  function wagesEarnedOnDate(employee, date) {
+    const hoursWorked = hoursWorkedOnDate(employee, date);
+    return hoursWorked * employee.payPerHour;
+  }
+  function allWagesFor(employee) {
+    const datesWorked = employee.timeInEvents.map((event) => event.date);
+    return datesWorked.reduce((total, date) => total + wagesEarnedOnDate(employee, date), 0);
+  }
+  function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find((employee) => employee.firstName === firstName);
+  }
+  function calculatePayroll(employeeRecords) {
+    return employeeRecords.reduce((totalPayroll, employee) => totalPayroll + allWagesFor(employee), 0);
+  }
+  
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
